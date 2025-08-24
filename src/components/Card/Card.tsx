8c293bc5a92601fe0323df1acb5e982c5cff5b7f
ayukoyamashita <ayuko.yamashita@kufusumai.co.jp>
2025-08-24 21:08:20 +0900

    カードクリックで拡大

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,5 +1,5 @@
+import { useState } from "react";
 import styles from "./Card.module.css";
-
 import { getCardById } from "../../utils/cardUtils";
 import { elementIcons } from "../../constants/cards";
 import type { Move } from "../../types/card";
@@ -24,40 +24,63 @@ type CardProps = PlayerCardProps | CpuCardProps;
 
 function Card({ playerType, cardId, currentHp, onAttack }: CardProps) {
   const card = getCardById(cardId);
+  const [inFocus, setInFocus] = useState(false);
 
   if (!card) {
     return <div className={styles.container}>カードが見つかりません</div>;
   }
 
-  const handleAttack = (move: Move) => {
-    if (playerType === "player") {
-      onAttack(move);
+  const handleAttack = (move: Move, event: React.MouseEvent) => {
+		event.stopPropagation();
+    if (playerType !== "player" || !inFocus) {
+      return;
     }
+		setInFocus(false);
+		onAttack(move);
+  };
+
+  const handleFocus = () => {
+    setInFocus(true);
+  };
+
+  const handleBlur = () => {
+    setInFocus(false);
   };
 
   return (
-    <div className={`${styles.container} ${styles[card.element]}`}>
-      <div className={styles.header}>
-        <div className={styles.name}>{card.name}</div>
-        <div className={styles.hp}>{currentHp}</div>
-        <div className={styles.element}>{elementIcons[card.element]}</div>
+    <>
+      <div
+        className={`${styles.background} ${inFocus ? styles.inFocus : ""}`}
+        onClick={handleBlur}
+      ></div>
+      <div
+        className={`${styles.container} ${styles[card.element]} ${
+          inFocus ? styles.inFocus : ""
+        }`}
+        onClick={handleFocus}
+      >
+        <div className={styles.header}>
+          <div className={styles.name}>{card.name}</div>
+          <div className={styles.hp}>{currentHp}</div>
+          <div className={styles.element}>{elementIcons[card.element]}</div>
+        </div>
+        <figure className={styles.image}></figure>
+        <ul className={styles.moves}>
+          {card.moves.map((move) => (
+            <li key={move.name} className={styles.move}>
+              <button
+                type="button"
+                onClick={(event) => handleAttack(move, event)}
+                disabled={playerType === "cpu"}
+              >
+                <span className={styles.moveName}>{move.name}</span>
+                <span className={styles.moveDamage}>{move.damage}</span>
+              </button>
+            </li>
+          ))}
+        </ul>
       </div>
-      <figure className={styles.image}></figure>
-      <ul className={styles.moves}>
-        {card.moves.map((move) => (
-          <li key={move.name} className={styles.move}>
-            <button
-              type="button"
-              onClick={() => handleAttack(move)}
-              disabled={playerType === "cpu"}
-            >
-              <span className={styles.moveName}>{move.name}</span>
-              <span className={styles.moveDamage}>{move.damage}</span>
-            </button>
-          </li>
-        ))}
-      </ul>
-    </div>
+    </>
   );
 }
 
