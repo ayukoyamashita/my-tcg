33609b460246df2d943f4fab0dcb0b0e4cc29ea1
ayukoyamashita <ayuko.yamashita@kufusumai.co.jp>
2025-08-24 18:39:37 +0900

    カードに色付け

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -2,22 +2,28 @@ import styles from "./Card.module.css";
 import { getCardById } from "../../utils/cardUtils";
 
 type CardProps = {
-	cardId: string;
-}
+  cardId: string;
+};
 
 function Card({ cardId }: CardProps) {
-	const card = getCardById(cardId);
-	
-	if (!card) {
-		return <div className={styles.container}>カードが見つかりません</div>;
-	}
-	
-	return <div className={styles.container}>
-		<div className={styles.name}>{card.name}</div>
-		<div className={styles.element}>{card.element}</div>
-		<div className={styles.hp}>{card.hp}</div>
-		<div className={styles.moves}>{card.moves.map((move) => move.name).join(", ")}</div>
-	</div>;
+  const card = getCardById(cardId);
+
+  if (!card) {
+    return <div className={styles.container}>カードが見つかりません</div>;
+  }
+
+  return (
+    <div className={`${styles.container} ${styles[card.element]}`}>
+      <div className={styles.header}>
+        <div className={styles.name}>{card.name}</div>
+        <div className={styles.hp}>{card.hp}</div>
+        <div className={styles.element}>{card.element}</div>
+      </div>
+      <div className={styles.moves}>
+        {card.moves.map((move) => move.name).join(", ")}
+      </div>
+    </div>
+  );
 }
 
 export default Card;
