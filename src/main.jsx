5475df7cb43be7bf5ab072625635b6f14bd32923
ayukoyamashita <ayuko.yamashita@kufusumai.co.jp>
2025-08-24 17:40:53 +0900

    Battleコンポーネント追加＆ディレクトリ構成整理

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import './styles/index.css'
+import Battle from './components/Battle/Battle.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Battle />
   </StrictMode>,
 )
