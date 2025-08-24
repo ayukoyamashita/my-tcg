8c87cd729ca30291345269cff4ebf5cf99d976a8
ayukoyamashita <ayuko.yamashita@kufusumai.co.jp>
2025-08-24 20:42:38 +0900

    攻撃する

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -4,24 +4,36 @@ import { getCardById } from "../../utils/cardUtils";
 import { elementIcons } from "../../constants/cards";
 import type { Move } from "../../types/card";
 
-type CardProps = {
+type BaseCardProps = {
   playerType: "player" | "cpu";
   cardId: string;
   currentHp: number;
   onAttack?: (move: Move) => void;
 };
 
-function Card({
-  playerType,
-  cardId,
-  currentHp,
-  onAttack,
-}: CardProps) {
+type PlayerCardProps = BaseCardProps & {
+  playerType: "player";
+  onAttack: (move: Move) => void;
+};
+
+type CpuCardProps = BaseCardProps & {
+  playerType: "cpu";
+};
+
+type CardProps = PlayerCardProps | CpuCardProps;
+
+function Card({ playerType, cardId, currentHp, onAttack }: CardProps) {
   const card = getCardById(cardId);
 
   if (!card) {
     return <div className={styles.container}>カードが見つかりません</div>;
   }
+
+  const handleAttack = (move: Move) => {
+    if (playerType === "player") {
+      onAttack(move);
+    }
+  };
 
   return (
     <div className={`${styles.container} ${styles[card.element]}`}>
@@ -34,7 +46,11 @@ function Card({
       <ul className={styles.moves}>
         {card.moves.map((move) => (
           <li key={move.name} className={styles.move}>
-            <button type="button" onClick={() => onAttack?.(move)}>
+            <button
+              type="button"
+              onClick={() => handleAttack(move)}
+              disabled={playerType === "cpu"}
+            >
               <span className={styles.moveName}>{move.name}</span>
               <span className={styles.moveDamage}>{move.damage}</span>
             </button>
