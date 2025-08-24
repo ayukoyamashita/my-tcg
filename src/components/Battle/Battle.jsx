29275db78092b3fb82f1451823cbb872400d16b0
ayukoyamashita <ayuko.yamashita@kufusumai.co.jp>
2025-08-24 18:01:59 +0900

    カード配置、css module導入

--- src/components/Battle/Battle.jsx
+++ src/components/Battle/Battle.jsx
@@ -1,7 +1,17 @@
-import "./Battle.css";
+import styles from "./Battle.module.css";
+import Card from "../Card/Card";
 
 function Battle() {
-	return <div className="battle">Battle</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.opponentContainer}>
+        <Card />
+      </div>
+      <div className={styles.playerContainer}>
+        <Card />
+      </div>
+    </div>
+  );
 }
 
 export default Battle;
--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,10 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   base: "/my-tcg/",
+	css: {
+    modules: {
+      localsConvention: 'camelCase',
+      generateScopedName: '[name]__[local]__[hash:base64:5]'
+    }
+  }
 });
