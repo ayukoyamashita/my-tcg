38a6fae53db4f0f0031628526c767d525ad7c20e
ayukoyamashita <ayuko.yamashita@kufusumai.co.jp>
2025-08-24 19:05:05 +0900

    ターンの管理

--- src/components/Battle/Battle.tsx
+++ src/components/Battle/Battle.tsx
@@ -1,9 +1,14 @@
 import styles from "./Battle.module.css";
 import Card from "../Card/Card";
+import { useState } from "react";
 
 function Battle() {
+  const [isPlayerTurn, setIsPlayerTurn] = useState(true); // プレイヤーのターンから開始
   return (
     <div className={styles.container}>
+      <div className={styles.turnIndicator}>
+        {isPlayerTurn ? "あなたのターン" : "相手のターン"}
+      </div>
       <div className={styles.opponentContainer}>
         <Card cardId="nyaoha" />
       </div>
